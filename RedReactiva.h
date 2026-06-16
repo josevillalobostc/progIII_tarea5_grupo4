523377d3bc8aa985f8b1cb89258c0d0f7e1fe47f
catitooou <catherine.lopez@utec.edu.pe>
2026-06-16 14:42:48 -0500

    cambios de persona 3

--- RedReactiva.h
+++ RedReactiva.h
@@ -1,24 +1,54 @@
 #pragma once
 #include "Blockchain.h"
 #include <vector>
+#include <iostream>
+#include <string>
 
+     //Interfaz del Observador
 struct MesaElectoralObserver {
   virtual void update(Block *nuevoBloque) = 0;
   virtual ~MesaElectoralObserver() = default;
 };
 
+    //Nodos:Observadores)
 class MesaElectoral : public MesaElectoralObserver {
 private:
-  Blockchain *blockchain;
+  std::string nombreMesa;
 
 public:
-  void update(Block *nuevoBloque) override {}
+  MesaElectoral(std::string nombre) : nombreMesa(nombre) {}
+
+  void update(Block *nuevoBloque) override {
+    std::cout << "[" << nombreMesa << "] Notificacion recibida. Analizando bloque " << nuevoBloque->ind << "...\n";
+    
+    //simulamos validacion 
+    if (nuevoBloque->current_hash == nuevoBloque->calculateHash()) {
+      std::cout << " -> [" << nombreMesa << "] Exito, el bloque fue valido. Registrando consenso .\n";
+    } else {
+      std::cout << " -> [" << nombreMesa << "] Alerta, el bloque es invalido o esta alterado. Rechazando .\n";
+    }
+  }
 };
 
+// Canal Central 
 class CentroElectoralSubject {
-  std::vector<MesaElectoral *> mesasRegistradas;
+private:
+  // Utilizamos la interfaz para respetar el polimorfismo del patron observer
+  std::vector<MesaElectoralObserver *> mesasRegistradas;
 
 public:
-  void attach(MesaElectoral *mesa) {}
-  void notificarNuevoBloque(Block *b) {}
+  void attach(MesaElectoralObserver *mesa) {
+    mesasRegistradas.push_back(mesa);
+  }
+
+  void notificarNuevoBloque(Block *b) {
+    std::cout << "\n---------------------------------------------------------\n";
+    std::cout << "[RED ELECTORAL] Transmitiendo bloque minado a " 
+              << mesasRegistradas.size() << " mesas\n";
+    std::cout << "-------------------------------------------------------------------\n";
+    
+    for (MesaElectoralObserver *mesa : mesasRegistradas) {
+      mesa->update(b);
+    }
+  }
 };
--- main.cpp
+++ main.cpp
@@ -1,4 +1,54 @@
 #include "RedReactiva.h"
+#include "Block.h"
+#include "Blockchain.h"
+#include "Blockchain.h"
+#include <iostream>
+
 using namespace std;
 
-int main() {}
+int main() {
+
+cout << "Iniciando sistema de votacion blockchain.....(cargando)\n\n";
+
+//  Instanciando subject
+    CentroElectoralSubject redElectoral;
+
+//  Instanciando las mesas observer
+    MesaElectoral mesaNorte("Mesa Norte");
+    MesaElectoral mesaSur("Mesa Sur");
+    MesaElectoral mesaCentro("Mesa Centro");
+
+    // Suscribir las mesas a la red
+    redElectoral.attach(&mesaNorte);
+    redElectoral.attach(&mesaSur);
+    redElectoral.attach(&mesaCentro);
+
+    //Obtener la Blockchain única que es singleton
+    Blockchain& cadenaNacional = Blockchain::getInstance();
+
+    //Simular la creación de votos en un nuevo bloque
+    //datos ficticios 
+    cout << "\n[SISTEMA] Recopilando nuevos votos...(cargando)\n";
+    Block* nuevoBloque = new Block();
+    nuevoBloque->votos.push_back({"ID_67677", "Candidato A"});
+    nuevoBloque->votos.push_back({"ID_11111", "Candidato B"});
+    nuevoBloque->votos.push_back({"ID_88888", "Candidato A"});
+
+
+    cadenaNacional.addBlock(nuevoBloque);
+
+    //Minería prueba de trabajo para sellar la urna
+    cout << "\n[SISTEMA] Sellando la urna (Mineria iniciada)...\n";
+    int dificultad = 3; // queremos  que el hash empiece con "000"
+    cadenaNacional.mineBlock(dificultad);
+
+    // notificamos a las demás mesas mediante el Patrón Observer
+    redElectoral.notificarNuevoBloque(nuevoBloque);
+
+    cout << "\n[SISTEMA] Auditoria final de la cadena de bloques:\n";
+    cadenaNacional.isChainValid();
+
+    cout << "\nFin de la simulacion .\n";
+
+    return 0;
+}
